7a121087d1d95c30d6d042c9d6ed4d01edf00170
Josh Wilensky <joshwilensky@gmail.com>
2022-03-16 14:43:12 -0400

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserResults from "../component/users/UserResults";
+import UserSearch from "../component/users/UserSearch";
+
+function Home() {
+  return (
+    <div>
+      <UserSearch />
+      <UserResults />
+    </div>
+  );
+}
+
+export default Home;
